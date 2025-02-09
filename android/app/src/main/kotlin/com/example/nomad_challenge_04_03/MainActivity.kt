dd4bac59b98381aacb0d3d001798467648c5e173
zz-yoon <yun.zz.del@gmail.com>
2025-02-10 00:55:09 +0900

    🎉  Nomad third task of week4
     Nomad third task of week4

---
+++ android/app/src/main/kotlin/com/example/nomad_challenge_04_03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nomad_challenge_04_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/bottom_navigation/view/bottom_navigation_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
